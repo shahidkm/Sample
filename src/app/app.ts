3f3c8e23138cbb3bf56f4d0d9a10505ee4a5f92f
shahidkm <kmshahid432@gmail.com>
2025-07-02 08:57:58 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MovieCard } from './shared/movie-card/movie-card';
+
 import { MovieList } from './pages/movie-list/movie-list';
 import { UserRegistration } from './pages/user-registration/user-registration';
-import { LoginUser } from './pages/user-login/user-login';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MovieCard, MovieList,UserRegistration,LoginUser],
+  imports: [RouterOutlet,MovieList,UserRegistration],
   templateUrl: './app.html',
   styleUrls: ['./app.css'] // ✅ Fixed key here
 })
--- src/app/pages/user-login/user-login.ts
+++ src/app/pages/user-login/user-login.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
-import { User } from '../../models/user.model';
-import { UserLogin } from '../../services/user-login';
-@Component({
-  selector: 'app-user-login',
-  imports: [],
-  templateUrl: './user-login.html',
-  styleUrl: './user-login.css'
-})
-export class LoginUser {
-email:string="";
-password:string="";
-    constructor(private userLogin:UserLogin ){}
-  async onSubmit():Promise<void>{
-    try{
-  const response=await this.userLogin.checkUserExists(this.email,this.password);
-  console.log(response);
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { UserLogin } from '../../services/user-login';
+// @Component({
+//   selector: 'app-user-login',
+//   imports: [FormsModule],
+//   templateUrl: './user-login.html',
+//   styleUrl: './user-login.css'
+// })
+// export class LoginUser {
+// email:string="";
+// password:string="";
+//     constructor(private userLogin:UserLogin ){}
+//   async onSubmit():Promise<void>{
+//     try{
+//   const response=await this.userLogin.checkUserExists(this.email,this.password);
+//   console.log(response);
   
-    }
-    catch(error){
-      console.log(error);
+//     }
+//     catch(error){
+//       console.log(error);
       
-    }
-  }
-}
+//     }
+//   }
+// }
